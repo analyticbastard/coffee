ecab314bf965eb144d5ce98d21555c7d018a3f4d
Analytic <albrecht.allenstein@gmail.com>
2016-03-20 20:50:38 +0100

    Coffee images for flat white and black americano types

--- src/main/clj/server/types.clj
+++ src/main/clj/server/types.clj
@@ -6,7 +6,7 @@
   [{:name "Latte"
     :img (str images-common-url "coffee-latte.jpg")}
    {:name "Black Americano"
-    :img (str images-common-url "coffee-latte.jpg")}
+    :img (str images-common-url "coffee-black-americano.jpg")}
    {:name "Flat White"
-    :img (str images-common-url "coffee-latte.jpg")}
+    :img (str images-common-url "coffee-flat-white.jpg")}
    ])
